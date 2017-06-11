1f350cafc68b32bda9bc4b28c75ba2332fb9ffc4
John Lee <drumwolf@Johns-MacBook-Pro.local>
2017-06-11 08:31:18 -0700

    weed out images with invalid exif data

--- ifoto.js
+++ ifoto.js
@@ -21,16 +21,16 @@ function processJPG(file) {
 	}
 }
 function renameJPG(file, exifData) {
+	// relevant exif data
 	const jpgModel = exifData.image.Model;
 	const jpgNumber = file.match(/\d{4}/)[0];
 	const jpgModifyDate = exifData.image.ModifyDate;
 	const jpgCreateDate = exifData.exif.CreateDate;
 	const jpgDateTimeOriginal = exifData.exif.DateTimeOriginal;
-	console.log(`=============================================`);
-	console.log(file);
-	console.log(`Model:   ${jpgModel}`);
-	console.log(`Nummer:  ${jpgNumber}`);
-	console.log(`ModifyDate:       ${jpgModifyDate}`);
-	console.log(`CreateDate:       ${jpgCreateDate}`);
-	console.log(`DateTimeOriginal: ${jpgDateTimeOriginal}`);
+	// test to see if exif data is valid
+	const hasExifData = (jpgModel && jpgModifyDate === jpgCreateDate && jpgModifyDate === jpgDateTimeOriginal);
+	// rename file if exif data is valid
+	if (hasExifData) {
+		console.log(`${file} =======> ${jpgModel} / ${jpgNumber} / ${jpgCreateDate}`)
+	}
 }
