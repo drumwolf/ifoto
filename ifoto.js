2ca5b5c559d098f5c4ca1ce17462742e6b046f05
John Lee <drumwolfmini@Johns-Mac-mini.local>
2017-08-27 10:52:47 -0700

    implemented functionality to rename PNGs and MOVs

--- ifoto.js
+++ ifoto.js
@@ -3,10 +3,33 @@ const ExifImage = require('exif').ExifImage;
 const dir = './files/';
 
 fs.readdir(dir, (err, files) => {
-	const jpegs = files.filter( file => file.slice(-4).toUpperCase() === '.JPG' );
+	const jpegs = [];
+	const pngs  = [];
+	const movs  = [];
+	files.forEach( file => {
+		const ext = file.slice(-4).toUpperCase();
+		if (ext === '.JPG') { jpegs.push(file) }
+		if (ext === '.PNG') { pngs.push(file) }
+		if (ext === '.MOV') { movs.push(file) }
+	});
+	pngs.forEach(processNonEXIF)
+	movs.forEach(processNonEXIF)
 	jpegs.forEach(processJPG);
 });
 
+function processNonEXIF(file) {
+	fs.stat(dir + file, (err,stat) => {
+		const fileNum  = file.match(/\d{4}/);
+		const fileExt  = file.slice(-4).toUpperCase();
+		const fileTime = stat.mtime;
+		const myear  = fileTime.getYear() + 1900;
+		const mmonth = ('0' + (fileTime.getMonth() + 1)).slice(-2);
+		const mdate  = ('0' + fileTime.getDate()).slice(-2);
+		const mtime  = fileTime.toString().split(" ")[4].replace(/:/g, '.');
+		const newFilename = `XXXXX${fileNum}__${myear}-${mmonth}-${mdate} (${mtime})${fileExt}`;
+		renameFile(file, newFilename);
+	});
+}
 function processJPG(file) {
 	try {
 		new ExifImage({ image : dir + file }, function (error, exifData) {
